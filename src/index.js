e17b4d6837494b1d74d99ecd1fafaf3699213fd9
Thomas Cheng <thomascheng81@gmail.com>
2017-06-04 06:28:29 -0400

    Draw a tetris shape and add keyboard controls

--- src/index.js
+++ src/index.js
@@ -1,36 +1,81 @@
-import { Scene, PerspectiveCamera, BoxGeometry, MeshBasicMaterial, Mesh, WebGLRenderer } from "three";
+import {
+  Scene,
+  PerspectiveCamera,
+  BoxBufferGeometry,
+  MeshBasicMaterial,
+  Mesh,
+  Group,
+  WebGLRenderer
+} from "three";
 
-let scene, camera, renderer;
-let geometry, material, mesh;
+const UNIT_SIZE = 80;
+
+const scene = new Scene();
+const renderer = new WebGLRenderer();
+const camera = new PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  1,
+  10000
+);
+const unit = new BoxBufferGeometry(UNIT_SIZE, UNIT_SIZE, UNIT_SIZE);
+const material = new MeshBasicMaterial({ color: 0xff7f50, wireframe: true });
+const figure = new Group();
+let xRotation = 0;
+let yRotation = 0;
+
+const cubeCoordinates = [[0, 0, 0], [-1, 0, 0], [-1, 1, 0], [1, 0, 0]];
+
+const getPosition = coor => [
+  UNIT_SIZE * coor[0],
+  UNIT_SIZE * coor[1],
+  UNIT_SIZE * coor[2]
+];
+
+const handleKeyDown = evt => {
+  switch (evt.code) {
+    case "ArrowDown":
+      evt.preventDefault();
+      xRotation = xRotation + 1;
+      break;
+    case "ArrowUp":
+      evt.preventDefault();
+      xRotation = xRotation - 1;
+      break;
+    case "ArrowLeft":
+      evt.preventDefault();
+      yRotation = yRotation - 1;
+      break;
+    case "ArrowRight":
+      evt.preventDefault();
+      yRotation = yRotation + 1;
+      break;
+    default:
+      break;
+  }
+};
 
 const init = () => {
-  scene = new Scene();
-
-  camera = new PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    1,
-    10000
-  );
   camera.position.z = 1000;
-
-  geometry = new BoxGeometry(200, 200, 200);
-  material = new MeshBasicMaterial({ color: 0xff0000, wireframe: true });
-
-  mesh = new Mesh(geometry, material);
-  scene.add(mesh);
-
-  renderer = new WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
-
   document.body.appendChild(renderer.domElement);
+
+  window.addEventListener("keydown", handleKeyDown);
+
+  cubeCoordinates.forEach(coordinates => {
+    const cube = new Mesh(unit, material);
+    cube.position.set(...getPosition(coordinates));
+    figure.add(cube);
+  });
+
+  scene.add(figure);
 };
 
 const animate = () => {
   requestAnimationFrame(animate);
 
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.02;
+  figure.rotation.x += 0.2 * (xRotation * 0.5 * Math.PI - figure.rotation.x);
+  figure.rotation.y += 0.2 * (yRotation * 0.5 * Math.PI - figure.rotation.y);
 
   renderer.render(scene, camera);
 };
