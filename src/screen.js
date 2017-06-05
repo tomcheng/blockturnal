4519d45b4fc0dfd1354ab7db9eb2e73094b907eb
Thomas Cheng <thomascheng81@gmail.com>
2017-06-05 06:55:45 -0400

    Add a hole to the screen

--- src/screen.js
+++ src/screen.js
@@ -1,18 +1,29 @@
-import { Shape, ExtrudeGeometry, Mesh } from "three";
-import { SCREEN_SIZE, MATERIAL, INITIAL_SCREEN_DISTANCE } from "./constants";
+import { Shape, Path, ExtrudeGeometry, Mesh, Vector2 } from "three";
+import { SCREEN_SIZE, MATERIAL, INITIAL_SCREEN_DISTANCE, UNIT_SIZE } from "./constants";
 
 const extrudeSettings = { amount: 1, bevelEnabled: false };
 
 class Screen {
   constructor() {
-    const screen = new Shape();
-    screen.moveTo(0.5 * SCREEN_SIZE, 0.5 * SCREEN_SIZE);
-    screen.lineTo(0.5 * SCREEN_SIZE, -0.5 * SCREEN_SIZE);
-    screen.lineTo(-0.5 * SCREEN_SIZE, -0.5 * SCREEN_SIZE);
-    screen.lineTo(-0.5 * SCREEN_SIZE, 0.5 * SCREEN_SIZE);
-    screen.lineTo(0.5 * SCREEN_SIZE, 0.5 * SCREEN_SIZE);
+    const shape = new Shape([
+      new Vector2(-0.5 * SCREEN_SIZE, 0.5 * SCREEN_SIZE),
+      new Vector2(-0.5 * SCREEN_SIZE, -0.5 * SCREEN_SIZE),
+      new Vector2(0.5 * SCREEN_SIZE, -0.5 * SCREEN_SIZE),
+      new Vector2(0.5 * SCREEN_SIZE, 0.5 * SCREEN_SIZE)
+    ]);
 
-    this.mesh = new Mesh(new ExtrudeGeometry(screen, extrudeSettings), MATERIAL);
+    const hole = new Path([
+      new Vector2(-1.5 * UNIT_SIZE, -0.5 * UNIT_SIZE),
+      new Vector2(-1.5 * UNIT_SIZE, 1.5 * UNIT_SIZE),
+      new Vector2(-0.5 * UNIT_SIZE, 1.5 * UNIT_SIZE),
+      new Vector2(-0.5 * UNIT_SIZE, 0.5 * UNIT_SIZE),
+      new Vector2(1.5 * UNIT_SIZE, 0.5 * UNIT_SIZE),
+      new Vector2(1.5 * UNIT_SIZE, -0.5 * UNIT_SIZE)
+    ]);
+
+    shape.holes = [hole];
+
+    this.mesh = new Mesh(new ExtrudeGeometry(shape, extrudeSettings), MATERIAL);
     this.mesh.position.set(0, 0, -INITIAL_SCREEN_DISTANCE);
   }
 }
