ef4568f05b5036db4215dd9a5669f1ac5a35485f
Thomas Cheng <thomascheng81@gmail.com>
2017-06-06 19:41:19 -0400

    Add sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,9 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
   },
+  devtool: "source-map",
   devServer: {
     contentBase: path.join(__dirname, "public")
   },
