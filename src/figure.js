52817cd3e6068792d1874901e3b5f25f83000822
Thomas Cheng <thomascheng81@gmail.com>
2017-06-06 23:09:13 -0400

    Add method to get current figure projection

--- src/figure.js
+++ src/figure.js
@@ -1,29 +1,38 @@
-import { Group, Quaternion, Euler, Mesh, BoxBufferGeometry, Matrix4, Vector3 } from "three";
+import {
+  Group,
+  Quaternion,
+  Euler,
+  Mesh,
+  BoxBufferGeometry,
+  Vector3
+} from "three";
 import { UNIT_SIZE, MATERIAL, ROTATION_DECAY } from "./constants";
 import { rotate } from "./rotations";
 import { getDimensions } from "./measurements";
+import { getProjection } from "./projections";
 
 const unit = new BoxBufferGeometry(UNIT_SIZE, UNIT_SIZE, UNIT_SIZE);
 
 class Figure {
   constructor() {
-    const cubeCoordinates = [
+    const unitCoordinates = [
+      new Vector3(-1, 0, 0),
       new Vector3(0, 0, 0),
-      new Vector3(0, 1, 0),
-      new Vector3(-1, 1, 0),
-      new Vector3(1, 1, 0),
-      new Vector3(1, 1, 1),
+      new Vector3(1, 0, 0)
     ];
     const mesh = new Group();
     const desiredRotation = new Quaternion();
-    const { width, height, depth, x, y, z } = getDimensions(cubeCoordinates);
-    const rotationMatrix = new Matrix4();
+    const { width, height, depth, x, y, z } = getDimensions(unitCoordinates);
 
     desiredRotation.setFromEuler(new Euler(0, 0, 0));
 
-    cubeCoordinates.forEach(coordinates => {
+    unitCoordinates.forEach(c => {
       const cube = new Mesh(unit, MATERIAL);
-      cube.position.set(coordinates.x * UNIT_SIZE, coordinates.y * UNIT_SIZE, coordinates.z * UNIT_SIZE);
+      cube.position.set(
+        c.x * UNIT_SIZE,
+        c.y * UNIT_SIZE,
+        c.z * UNIT_SIZE
+      );
       cube.translateX(-0.5 * width - x);
       cube.translateY(-0.5 * height - y);
       cube.translateZ(-0.5 * depth - z);
@@ -32,11 +41,17 @@ class Figure {
 
     this.mesh = mesh;
 
-    this.coordinates = cubeCoordinates;
+    this.coordinates = unitCoordinates;
 
     this.rotate = direction => {
       rotate(direction, desiredRotation);
-      rotationMatrix.makeRotationFromQuaternion(desiredRotation);
+    };
+
+    this.getCurrentProjection = () => {
+      const rotatedCoordinates = unitCoordinates.map(c =>
+        c.clone().applyQuaternion(desiredRotation).round()
+      );
+      return getProjection({ axis: "z", coordinates: rotatedCoordinates });
     };
 
     this.update = () => {
