fae5a4b88520033e2d4e37e37e6d41764f672c2d
Thomas Cheng <thomascheng81@gmail.com>
2017-06-07 22:45:16 -0400

    Add a background color and fog to the scene

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import { Scene, WebGLRenderer } from "three";
-import Camera from "./camera";
+import * as T from "three";
+import { INITIAL_SCREEN_DISTANCE } from "./constants"
+;import Camera from "./camera";
 import Figure from "./figure";
 import ScreenManager from "./screenManager";
 
-const scene = new Scene();
-const renderer = new WebGLRenderer();
+const scene = new T.Scene();
+const renderer = new T.WebGLRenderer();
 
 const camera = new Camera();
 const figure = new Figure();
@@ -82,12 +83,15 @@ const animate = () => {
 (() => {
   window.addEventListener("keydown", handleKeyDown);
 
-  screenManager.setNewHole(figure.getRandomProjection());
+  scene.background = new T.Color(0xd1edfa);
+  scene.fog = new T.Fog(0xd1edfa, 1, 1.3 * INITIAL_SCREEN_DISTANCE);
 
-  scene.add(figure.mesh);
+  screenManager.setNewHole(figure.getRandomProjection());
   screenManager.getScreens().forEach(s => {
     scene.add(s.mesh);
   });
+
+  scene.add(figure.mesh);
 
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
