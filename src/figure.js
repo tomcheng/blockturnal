35b77efcb52d8c8d559e572f60a23fa5c929faa4
Thomas Cheng <thomascheng81@gmail.com>
2017-06-09 08:16:01 -0400

    Refactor figure

--- src/figure.js
+++ src/figure.js
@@ -7,91 +7,86 @@ import {
   ROTATION_DECAY,
   INITIAL_FIGURE
 } from "./constants";
-import { rotate } from "./rotations";
+import { rotate as _rotate } from "./rotations";
 import { getDimensions } from "./measurements";
-import { getProjection, getRandomProjection } from "./projections";
+import { getProjection, getRandomProjection as _getRandomProjection } from "./projections";
 import { addRandomUnit } from "./addUnit";
 
 const unit = new BoxBufferGeometry(UNIT_SIZE, UNIT_SIZE, UNIT_SIZE);
+const mesh = new Group();
+const desiredRotation = new Quaternion();
+let unitCoordinates = [].concat(INITIAL_FIGURE);
+let cubes = [];
 
 class Figure {
   constructor() {
-    let unitCoordinates = [].concat(INITIAL_FIGURE);
-    let cubes = [];
-    const mesh = new Group();
-    const desiredRotation = new Quaternion();
-
     desiredRotation.setFromEuler(new Euler(0, 0, 0));
 
-    const generateCubesAndMesh = () => {
-      cubes.forEach(cube => {
-        mesh.remove(cube);
-      });
-
-      const { width, height, depth, x, y, z } = getDimensions(unitCoordinates);
-
-      cubes = unitCoordinates.map(c => {
-        const cube = new Mesh(unit, MATERIAL);
-        cube.position.set(c.x * UNIT_SIZE, c.y * UNIT_SIZE, c.z * UNIT_SIZE);
-        cube.translateX(-0.5 * width - x);
-        cube.translateY(-0.5 * height - y);
-        cube.translateZ(-0.5 * depth - z);
-        return cube;
-      });
-
-      cubes.forEach(cube => {
-        mesh.add(cube);
-      });
-
-      this.maxDimension = max([width, height, depth]);
-    };
-
-    generateCubesAndMesh();
+    this._generateCubesAndMesh();
 
     this.mesh = mesh;
-
-    this.rotate = direction => {
-      rotate(direction, desiredRotation);
-    };
-
-    this.getCurrentProjection = () => {
-      const rotatedCoordinates = unitCoordinates.map(c =>
-        c.clone().applyQuaternion(desiredRotation).round()
-      );
-      return getProjection({ axis: "z", coordinates: rotatedCoordinates });
-    };
-
-    this.getRandomProjection = () => {
-      return getRandomProjection(unitCoordinates);
-    };
-
-    this.turnRed = () => {
-      cubes.forEach(cube => {
-        cube.material = LOSER_MATERIAL;
-      });
-    };
-
-    this.reset = () => {
-      unitCoordinates = [].concat(INITIAL_FIGURE);
-      generateCubesAndMesh();
-    };
-
-    this.addBlocks = () => {
-      addRandomUnit(unitCoordinates);
-      generateCubesAndMesh();
-    };
-
-    this.getBlockCount = () => unitCoordinates.length;
-
-    this.update = () => {
-      Quaternion.slerp(
-        mesh.quaternion,
-        desiredRotation,
-        mesh.quaternion,
-        ROTATION_DECAY
-      );
-    };
   }
+
+  rotate = direction => {
+    _rotate(direction, desiredRotation);
+  };
+
+  getCurrentProjection = () => {
+    const rotatedCoordinates = unitCoordinates.map(c =>
+      c.clone().applyQuaternion(desiredRotation).round()
+    );
+    return getProjection({ axis: "z", coordinates: rotatedCoordinates });
+  };
+
+  getRandomProjection = () => _getRandomProjection(unitCoordinates);
+
+  addBlocks = () => {
+    addRandomUnit(unitCoordinates);
+    this._generateCubesAndMesh();
+  };
+
+  turnRed = () => {
+    cubes.forEach(cube => {
+      cube.material = LOSER_MATERIAL;
+    });
+  };
+
+  reset = () => {
+    unitCoordinates = [].concat(INITIAL_FIGURE);
+    this._generateCubesAndMesh();
+  };
+
+  update = () => {
+    Quaternion.slerp(
+      mesh.quaternion,
+      desiredRotation,
+      mesh.quaternion,
+      ROTATION_DECAY
+    );
+  };
+
+  _generateCubesAndMesh = () => {
+    cubes.forEach(cube => {
+      mesh.remove(cube);
+    });
+
+    const { width, height, depth, x, y, z } = getDimensions(unitCoordinates);
+
+    cubes = unitCoordinates.map(c => {
+      const cube = new Mesh(unit, MATERIAL);
+      cube.position.set(c.x * UNIT_SIZE, c.y * UNIT_SIZE, c.z * UNIT_SIZE);
+      cube.translateX(-0.5 * width - x);
+      cube.translateY(-0.5 * height - y);
+      cube.translateZ(-0.5 * depth - z);
+      return cube;
+    });
+
+    cubes.forEach(cube => {
+      mesh.add(cube);
+    });
+
+    this.maxDimension = max([width, height, depth]);
+  };
 }
 
 export default Figure;
